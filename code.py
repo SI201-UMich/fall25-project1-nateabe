e165d6293a48ed5464532d25eb7869248561ab17
Nate Abraham <nateabraham@mac.mynetworksettings.com>
2025-10-16 14:36:41 -0400

    final

--- code.py
+++ code.py
@@ -1,18 +1,25 @@
+#Nate Abraham
+#1879 4472
+#nateabra@umich.edu
+#I worked alone
+#I used GenAi to help me write my functions and fix errors when I was stuck
+
+
 import csv
 
 def read_csv_to_dict(filename):
-    """Reads a CSV file and returns a list of dictionaries."""
+  
     with open(filename, "r") as file:
         reader = csv.DictReader(file)
         data = [row for row in reader]
     return data
 
 data = read_csv_to_dict("crop_yield.csv")
-print(len(data))  # Number of rows
-print(data[0])    # First entry
+print(data[0])    
+print(len(data))  
 
 def avg_rainfall_by_fertilizer(data):
-    """Returns average rainfall for fertilizer used and not used."""
+
     total_with = 0
     count_with = 0
     total_without = 0
@@ -29,13 +36,13 @@ def avg_rainfall_by_fertilizer(data):
             total_without += rainfall
             count_without += 1
 
-    avg_with = total_with / count_with if count_with else 0
-    avg_without = total_without / count_without if count_without else 0
+    avg_with = total_with / count_with
+    avg_without = total_without / count_without 
 
     return avg_with, avg_without
 
 def avg_yield_by_fertilizer(data):
-    """Returns average yield for fertilizer used and not used."""
+
     total_with = 0
     count_with = 0
     total_without = 0
@@ -52,13 +59,13 @@ def avg_yield_by_fertilizer(data):
             total_without += yield_val
             count_without += 1
 
-    avg_with = total_with / count_with if count_with else 0
-    avg_without = total_without / count_without if count_without else 0
+    avg_with = total_with / count_with
+    avg_without = total_without / count_without
 
     return avg_with, avg_without
 
 def write_results_to_file(rainfall_result, yield_result, filename="results.txt"):
-    """Writes the analysis results to a text file."""
+
     with open(filename, "w") as file:
         file.write("Crop Yield Data Analysis\n")
         file.write("=========================\n\n")
